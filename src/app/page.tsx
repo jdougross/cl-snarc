cdfa33b39736c39dc6c3ff3369489091ca302f2f
Doug Ross <doug@Dougs-Mac-mini-18579.lan>
2024-05-16 17:24:58 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Text } from "@chakra-ui/react";
 import { MerchVolunteers } from "./merch-volunteers";
 
 import { auth } from "@/app/auth";
@@ -11,6 +11,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ChakraProvider>
         <SignIn />
+        {session && <Text>{JSON.stringify(session.user?.email)}</Text>}
         {session && <MerchVolunteers />}
       </ChakraProvider>
     </main>
